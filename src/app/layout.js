d7025d7fef86c33b8b5b2826be7c178d9ada1b3d
Arafatullah Arshad <arafatullaharshad@gmail.com>
2026-05-04 17:00:31 +0600

    Sloving Big Problem and spent 4hours with that. PH support session are awesome

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Banner from "@/components/Banner";
 import TopAnimals from "@/components/TopAnimals";
-import Image from "next/image";
+
+
+
 
 export default function Home() {
   return (
