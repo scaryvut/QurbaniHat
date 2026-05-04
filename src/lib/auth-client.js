4c3e0a2b2dd4ef9f1abe1c4cbad16cd4223a4133
Arafatullah Arshad <arafatullaharshad@gmail.com>
2026-05-04 23:27:21 +0600

    Change

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://agent-69f8a5beade440152525fc63--qurbanihat-today.netlify.app"
+    baseURL: "https://qurbani-hat-bazar.vercel.app"
 })
 export const { signIn, signUp, useSession } = createAuthClient()
